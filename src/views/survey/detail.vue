65da4f4e5375a1768d36a5e4c17f41f7d7f17e81
mahy50 <934037783@qq.com>
2020-03-03 16:59:07 +0800

    添加问卷详情页面

--- src/views/survey/detail.vue
+++ src/views/survey/detail.vue
@@ -1,20 +1,50 @@
 <template>
   <div class="app-container">
-    问卷详情
+    <h2>{{ survey.title }}</h2>
+    <div v-if="survey.tips" class="form-tips">{{ survey.tips }}</div>
+    <el-form ref="formInfo" :model="formInfo" label-width="10em" label-position="right">
+      <div v-for="(section, key) in survey.sections" :key="key">
+        <h3 v-if="section.title" class="section-title">{{ section.title }}</h3>
+        <el-row class="survey-section">
+          <form-item
+            v-for="(item, itemKey) in section.questions"
+            :key="itemKey"
+            :item-key="itemKey"
+            :config="item"
+          />
+        </el-row>
+      </div>
+    </el-form>
   </div>
 </template>
 
 <script>
+import FormItem from './components/FormItem/index'
+import surveyTable from './data/index'
 export default {
   name: 'SurveyDetail',
+  provide() {
+    return {
+      provideData: this
+    }
+  },
+  components: {
+    FormItem
+  },
   data() {
-    return {}
+    return {
+      formInfo: {},
+      survey: surveyTable
+    }
   },
   mounted() {},
-  methods: {}
+  methods: {
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .app-container {
+    color: $menuText;
+  }
 </style>
