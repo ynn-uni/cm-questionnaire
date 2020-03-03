313d62ee2feee6cf9292a07f8f280f705ee1e665
mahy50 <934037783@qq.com>
2020-03-03 14:28:27 +0800

    add; 添加contentEditor组件，内容编辑器组件

---
+++ src/components/ContentEditor/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="content-editor">
+    <el-input :value="value" :placeholder="placeholder" size="small" @input="handleChange" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {
+    handleChange(value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content-editor::v-deep {
+  & + .content-editor {
+    margin-top: 20px;
+  }
+  .el-input__inner {
+    border: 1px solid transparent;
+    &:hover {
+      border: 1px solid #c0c4cc;
+    }
+    &:focus {
+      border: 1px solid $--color-primary;
+    }
+  }
+}
+</style>
