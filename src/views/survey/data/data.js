3185eff55ab6d284f3af82bbaa58c7b057ba8fd0
mahy50 <934037783@qq.com>
2020-03-04 15:49:51 +0800

    add; 添加测试数据

---
+++ src/views/survey/data/data.js
@@ -0,0 +1,92 @@
+export default {
+  title: '问卷标题',
+  content: '',
+  suffix: '您已完成本次问卷，感谢您的帮助与支持',
+  questions: [
+    {
+      id: 'hImeAEuH',
+      title: '单选题标题',
+      type: 1,
+      options: [
+        {
+          id: 'AtJK6hlCC',
+          label: '选项1',
+          has_open: false
+        },
+        {
+          id: 'Pu9SjgE8m',
+          label: '选项2',
+          has_open: false
+        }
+      ]
+    },
+    {
+      id: 'ceNBfmul',
+      title: '多选题标题',
+      type: 2,
+      column: 3,
+      options: [
+        {
+          id: 'Qhd24vCg4',
+          label: '选项1',
+          has_open: false
+        },
+        {
+          id: '9IcmVi2G1',
+          label: '选项2',
+          has_open: false
+        },
+        {
+          id: 'Qhd24vCg3',
+          label: '选项1',
+          has_open: false
+        },
+        {
+          id: '9IcmVi2GR',
+          label: '选项2',
+          has_open: false
+        }
+      ]
+    },
+    {
+      id: 'sFhIr4va',
+      title: '填空题标题',
+      placeholder: '提示内容',
+      type: 3
+    },
+    {
+      id: '7qr8jTGs',
+      title: '多选题标题',
+      type: 2,
+      options: [
+        {
+          id: 'KgNiMB2GD',
+          label: '选项1',
+          has_open: false
+        },
+        {
+          id: 'K8vnajGoQ',
+          label: '选项2',
+          has_open: false
+        }
+      ]
+    },
+    {
+      id: 'XKZiP97MP',
+      title: '单选题标题',
+      type: 1,
+      options: [
+        {
+          id: 'ZDRfnIu2e',
+          label: '选项1',
+          has_open: false
+        },
+        {
+          id: 'gyefKr_XP',
+          label: '选项2',
+          has_open: false
+        }
+      ]
+    }
+  ]
+}
