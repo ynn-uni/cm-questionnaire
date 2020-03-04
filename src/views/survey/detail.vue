7b3c0d6840d858f3498c57379d2af4c2e842ff5e
mahy50 <934037783@qq.com>
2020-03-04 15:49:36 +0800

    update; 更新问卷详情页

--- src/views/survey/detail.vue
+++ src/views/survey/detail.vue
@@ -1,50 +1,31 @@
 <template>
   <div class="app-container">
-    <h2>{{ survey.title }}</h2>
-    <div v-if="survey.tips" class="form-tips">{{ survey.tips }}</div>
-    <el-form ref="formInfo" :model="formInfo" label-width="10em" label-position="right">
-      <div v-for="(section, key) in survey.sections" :key="key">
-        <h3 v-if="section.title" class="section-title">{{ section.title }}</h3>
-        <el-row class="survey-section">
-          <form-item
-            v-for="(item, itemKey) in section.questions"
-            :key="itemKey"
-            :item-key="itemKey"
-            :config="item"
-          />
-        </el-row>
-      </div>
-    </el-form>
+    <SurveyForm :survey="surveyData" />
   </div>
 </template>
 
 <script>
-import FormItem from './components/FormItem/index'
-import surveyTable from './data/index'
+import SurveyForm from './components/SurveyForm'
+import surveyData from './data/data'
+
 export default {
   name: 'SurveyDetail',
-  provide() {
-    return {
-      provideData: this
-    }
-  },
   components: {
-    FormItem
+    SurveyForm
   },
   data() {
     return {
-      formInfo: {},
-      survey: surveyTable
+      surveyData
     }
   },
   mounted() {},
-  methods: {
-  }
+  methods: {}
 }
 </script>
 
 <style lang="scss" scoped>
-  .app-container {
-    color: $menuText;
-  }
+.app-container {
+  background: #efefef;
+  overflow-y: scroll;
+}
 </style>
