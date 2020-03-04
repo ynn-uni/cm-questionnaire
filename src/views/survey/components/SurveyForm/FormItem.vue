cf97afdfcd3c9e7aba76db368e411e0b1d52a241
mahy50 <934037783@qq.com>
2020-03-04 15:49:20 +0800

    add; 添加表单详情组件

---
+++ src/views/survey/components/SurveyForm/FormItem.vue
@@ -0,0 +1,101 @@
+<template>
+  <el-form-item v-if="question.type" class="form-item" :prop="question.id" :rules="rules">
+    <template slot="label">
+      <h3>{{ question.title }}</h3>
+    </template>
+    <div v-if="question.type === 1">
+      <el-radio-group v-model="provideData.formInfo[question.id]">
+        <el-row :gutter="20">
+          <el-col v-for="item in question.options" :key="item.id" :span="24 / question.column">
+            <el-radio :label="item.id">{{ item.label }}</el-radio>
+          </el-col>
+        </el-row>
+      </el-radio-group>
+    </div>
+    <div v-if="question.type === 2">
+      <el-checkbox-group v-model="provideData.formInfo[question.id]">
+        <el-row :gutter="20">
+          <el-col v-for="item in question.options" :key="item.id" :span="24 / question.column">
+            <el-checkbox :label="item.id">{{ item.label }}</el-checkbox>
+          </el-col>
+        </el-row>
+      </el-checkbox-group>
+    </div>
+    <div v-if="question.type === 3">
+      <el-input v-model="provideData.formInfo[question.id]" :placeholder="question.placeholder">
+        <template v-if="question && question.append" slot="append">{{ question.append }}</template>
+      </el-input>
+    </div>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'FormItem',
+  props: {
+    question: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  },
+  inject: ['provideData'],
+  data() {
+    return {}
+  },
+  computed: {
+    rules() {
+      const { type } = this.question
+      const required = true
+      if (type === 1) {
+        return {
+          required,
+          message: '请选择',
+          trigger: 'change'
+        }
+      }
+      if (type === 2) {
+        return {
+          required,
+          type: 'array',
+          message: '请选择',
+          trigger: 'change'
+        }
+      }
+
+      if (type === 3) {
+        return {
+          required,
+          message: '请填写',
+          trigger: 'blur'
+        }
+      }
+      return {}
+    }
+  },
+  methods: {
+    handleChange(value) {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form-item::v-deep {
+  .el-form-item__label {
+    text-align: unset;
+    width: 100%;
+    h3 {
+      display: inline-block;
+      margin-bottom: 0;
+    }
+  }
+  .el-radio,
+  .el-checkbox {
+    line-height: 40px;
+  }
+  .el-input {
+    max-width: 50%;
+    margin-top: 10px;
+  }
+}
+</style>
---
+++ src/views/survey/components/SurveyForm/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="survey-form">
+    <h2>{{ survey.title }}</h2>
+    <div v-if="survey.content" class="form-tips">{{ survey.content }}</div>
+    <el-divider />
+    <el-form v-if="formInfo" ref="formInfo" :model="formInfo">
+      <el-row v-for="question in survey.questions" :key="question.id" class="survey-section">
+        <form-item :question="question" />
+      </el-row>
+    </el-form>
+    <el-divider />
+    <div v-if="survey.suffix" class="form-tips">{{ survey.suffix }}</div>
+    <div class="survey-form-action">
+      <el-button type="primary" size="medium">提交</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import FormItem from './FormItem'
+export default {
+  name: 'SurveyDetail',
+  provide() {
+    return {
+      provideData: this
+    }
+  },
+  components: {
+    FormItem
+  },
+  props: {
+    survey: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      formInfo: null
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.formInfo = this.defaultForm()
+    })
+  },
+  methods: {
+    defaultForm() {
+      const result = {}
+      if (!this.survey) return
+      this.survey.questions.forEach(question => {
+        const { id } = question
+        if (question.type === 2) { // checkbox
+          result[id] = []
+        } else {
+          result[id] = ''
+        }
+      })
+      return result
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.survey-form {
+  max-width: 800px;
+  padding: 30px;
+  margin: auto;
+  border-radius: 4px;
+  font-size: 14px;
+  background: #fff;
+
+  .form-tips {
+    margin: 30px 0;
+  }
+  .el-divider {
+    height: 2px;
+    background: $--color-primary;
+  }
+  .survey-form-action {
+    .el-button {
+      width: 120px;
+    }
+  }
+}
+</style>
