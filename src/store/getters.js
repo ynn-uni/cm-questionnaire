167d97e7ed5238c7b51628519488c00921b5059c
mahy50 <934037783@qq.com>
2020-03-10 14:27:37 +0800

    update; 添加角色role

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,6 +2,7 @@ const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
   token: state => state.user.token,
-  userInfo: state => state.user.userInfo
+  userInfo: state => state.user.userInfo,
+  role: state => state.user.userInfo && state.user.userInfo.type
 }
 export default getters
