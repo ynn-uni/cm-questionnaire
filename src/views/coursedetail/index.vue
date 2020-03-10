71857331a8baac19f8375cc20c12460ec8ced12d
mahy50 <934037783@qq.com>
2020-03-10 14:52:43 +0800

    update; 更新课程详情

--- src/views/coursedetail/index.vue
+++ src/views/coursedetail/index.vue
@@ -1,78 +1,45 @@
 <template>
   <div class="coursedetail app-container">
-    <div class="title">
-      课程详情
-    </div>
+    <div class="title">课程详情</div>
     <div class="item flex">
       <div class="item-left flex">
-        <img class="ci-img" :src="baseUrl+data.cover" alt="">
+        <img class="ci-img" :src="baseUrl+data.cover" alt>
         <div class="info">
           <div class="title flex justify-between align-center">
-            <div class="title-text">
-              {{ data.title }}
-            </div>
+            <div class="title-text">{{ data.title }}</div>
             <!-- <div v-permission="1" class="collect" :class="data.collect?'active':''" @click="handelCollect(data.id)">
               <i class="iconfont icon-shoucang1" />
               {{ data.collect?'取消收藏':'收藏' }}
-            </div> -->
+            </div>-->
           </div>
-          <div class="info-item">
-            授课老师：{{ data.truename }}
-          </div>
-          <div class="info-item">
-            课    时：{{ data.hour }}
-          </div>
-          <div class="info-item">
-            考核方式：{{ data.check }}
-          </div>
-          <div class="info-item">
-            学    分：{{ data.credit }}
-          </div>
+          <div class="info-item">授课老师：{{ data.truename }}</div>
+          <div class="info-item">课 时：{{ data.hour }}</div>
+          <div class="info-item">考核方式：{{ data.check }}</div>
+          <div class="info-item">学 分：{{ data.credit }}</div>
           <div v-permission="1" class="other-option flex justify-between">
-            <el-tag
-              v-if="data.status==1"
-              type="success"
-              effect="dark"
-            >
-              进行中
-            </el-tag>
-            <el-tag
-              v-if="data.status==2"
-              type="info"
-              effect="dark"
-            >
-              已结束
-            </el-tag>
-            <el-button v-if="data.join==0" v-permission="1" type="primary" class="btn-enter" @click="showAdd">
-              申请加入
-            </el-button>
-
+            <el-tag v-if="data.status==1" type="success" effect="dark">进行中</el-tag>
+            <el-tag v-if="data.status==2" type="info" effect="dark">已结束</el-tag>
+            <el-button
+              v-if="data.join==0"
+              v-permission="1"
+              type="primary"
+              class="btn-enter"
+              @click="showAdd"
+            >申请加入</el-button>
           </div>
           <div v-permission="2" class="other-option">
-            <el-tag
-              v-if="data.status==1"
-              type="success"
-              effect="dark"
-            >
-              进行中
-            </el-tag>
-            <el-tag
-              v-if="data.status==2"
-              type="info"
-              effect="dark"
-            >
-              已结束
-            </el-tag>
+            <el-tag v-if="data.status==1" type="success" effect="dark">进行中</el-tag>
+            <el-tag v-if="data.status==2" type="info" effect="dark">已结束</el-tag>
             <div v-permission="2" class="other-option flex justify-between">
-              <el-button v-if="data.status==1" type="primary" class="btn-enter" @click="getCode">
-                邀请学生
-              </el-button>
-              <el-button type="primary" class="btn-enter" @click="editCourse(data.id)">
-                编辑课程
-              </el-button>
+              <el-button
+                v-if="data.status==1"
+                type="primary"
+                class="btn-enter"
+                @click="getCode"
+              >邀请学生</el-button>
+              <el-button type="primary" class="btn-enter" @click="editCourse(data.id)">编辑课程</el-button>
             </div>
           </div>
-
         </div>
       </div>
       <el-dialog
@@ -104,45 +71,41 @@
         <input v-model="code" type="text" class="addcode">
         <span slot="footer" class="dialog-footer">
           <el-button @click.stop="dialogVisible1 = false">取 消</el-button>
-          <el-button
-            type="primary"
-            @click.stop="handelAddCourse"
-          >申请加入</el-button>
+          <el-button type="primary" @click.stop="handelAddCourse">申请加入</el-button>
         </span>
       </el-dialog>
       <div class="bref">
-        <div class="dis-title">
-          课程介绍
-        </div>
-        <div class="content">
-          {{ data.describe }}
-        </div>
+        <div class="dis-title">课程介绍</div>
+        <div class="content">{{ data.describe }}</div>
       </div>
     </div>
-    <div class="title">
-      课程问卷
-    </div>
-    <QuestionList />
-    <div v-permission="1" class="title">
-      课程同学
-    </div>
-    <div v-permission="2" class="title">
-      我的学生
-    </div>
+    <div v-if="data.questionnaires.length" class="title">课程问卷</div>
+    <el-row v-if="data.questionnaires.length">
+      <el-col v-for="item in data.questionnaires" :key="item.id" v-bind="column">
+        <QuestionItem
+          :detail="{
+            ctitle: data.title,
+            ...item
+          }"
+        />
+      </el-col>
+    </el-row>
+    <div v-permission="1" class="title">课程同学</div>
+    <div v-permission="2" class="title">我的学生</div>
     <ClassMate :students="data.students" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import QuestionList from '@/components/QuestionList'
+import QuestionItem from '@/components/QuestionItem'
 import ClassMate from './components/ClassMate'
 import { getCourseDetails, joinCourse } from '@/api/course'
 import { addCousetOrCancleCollectrse } from '@/api/collect'
 export default {
   name: 'CourseDetail',
   components: {
-    QuestionList,
+    QuestionItem,
     ClassMate
   },
   data() {
@@ -152,18 +115,23 @@ export default {
       dialogVisible: false,
       dialogVisible1: false,
       code: '',
-      baseUrl: process.env.VUE_APP_STATIC_IMG
+      baseUrl: process.env.VUE_APP_STATIC_IMG,
+      column: {
+        xs: 24,
+        sm: 12,
+        md: 8,
+        lg: 8,
+        xl: 8
+      }
     }
   },
   computed: {
-    ...mapGetters([
-      'name'
-    ])
+    ...mapGetters(['name'])
   },
 
   mounted() {
     const id = this.$route.query.id
-    getCourseDetails({ id }).then((res) => {
+    getCourseDetails({ id }).then(res => {
       this.data = res
     })
   },
@@ -189,7 +157,7 @@ export default {
     handelAddCourse() {
       if (this.code) {
         const code = this.code
-        joinCourse({ code }).then((res) => {
+        joinCourse({ code }).then(res => {
           this.data.join = 1
           this.$message.success('加入成功')
         })
@@ -199,112 +167,108 @@ export default {
       }
     },
     handelCollect(id) {
-      addCousetOrCancleCollectrse({ type: 1, xid: id }).then((res) => {
+      addCousetOrCancleCollectrse({ type: 1, xid: id }).then(res => {
         this.data.collect = this.data.collect === 1 ? 0 : 1
-        this.$message.success(this.data.collect === 1 ? '收藏成功' : '取消收藏成功')
+        this.$message.success(
+          this.data.collect === 1 ? '收藏成功' : '取消收藏成功'
+        )
       })
     }
-
   }
-
 }
 </script>
 
 <style lang="scss" scoped>
 .coursedetail {
-  .title{
+  .title {
     font-size: 20px;
     color: $textPrimary;
   }
-  .item{
+  .item {
     margin: 30px 0;
     padding: 20px;
     width: 100%;
     // height: 280px;
     flex-wrap: wrap;
-    box-shadow:2px 2px 8px 2px rgba(217,224,227,0.5);
-    .item-left{
+    box-shadow: 2px 2px 8px 2px rgba(217, 224, 227, 0.5);
+    .item-left {
       @include response-courseDetailWidth();
       //  height: 200px;
       border-right: 2px solid #ebebeb;
-        .ci-img{
-          width: 150px;
-          height: 230px;
-          margin-right: 20px;
-        }
-      .info{
+      .ci-img {
+        width: 150px;
+        height: 230px;
+        margin-right: 20px;
+      }
+      .info {
         width: 240px;
-        color:$textPrimary;
+        color: $textPrimary;
         line-height: 30px;
 
-          .title{
-            width: 100%;
-            .title-text{
-              font-size: 16px;
-            }
-            .collect{
-              font-size: 14px;
-              color: $textSecondary;
-              width: 80px;
-              cursor: pointer;
-              i{
-                font-size: 20px;
-                color:$textPrimary;
-              }
-            }
-            .active{
-                i{
-                  color:#FFEC0F;
-                }
-              }
+        .title {
+          width: 100%;
+          .title-text {
+            font-size: 16px;
           }
-          .info-item{
+          .collect {
             font-size: 14px;
+            color: $textSecondary;
+            width: 80px;
+            cursor: pointer;
+            i {
+              font-size: 20px;
+              color: $textPrimary;
+            }
           }
-          .other-option{
-            height: 30px;
-            margin-top: 8px;
+          .active {
+            i {
+              color: #ffec0f;
+            }
           }
-
         }
-
+        .info-item {
+          font-size: 14px;
+        }
+        .other-option {
+          height: 30px;
+          margin-top: 8px;
+        }
+      }
     }
-    .code{
-    margin: 0 auto;
-    width: 200px;
-    text-align: center;
-    padding-bottom: 2px;
-    border-bottom: 1px solid $textSecondary;
-  }
-  .addcode{
-    width: 200px;
-    height: 40px;
-    border-radius: 5px;
-    border:1px solid $textSecondary;
-    display: block;
-    margin: 0 auto;
-    padding-left: 5px;
-    outline: none;
-
-  }
-    .bref{
+    .code {
+      margin: 0 auto;
+      width: 200px;
+      text-align: center;
+      padding-bottom: 2px;
+      border-bottom: 1px solid $textSecondary;
+    }
+    .addcode {
+      width: 200px;
+      height: 40px;
+      border-radius: 5px;
+      border: 1px solid $textSecondary;
+      display: block;
+      margin: 0 auto;
+      padding-left: 5px;
+      outline: none;
+    }
+    .bref {
       @include response-courseDetailWidth();
-       height: 200px;
-      padding-left:20px;
+      height: 200px;
+      padding-left: 20px;
       color: $textPrimary;
       text-align: justify;
       line-height: 20px;
 
-      .dis-title{
+      .dis-title {
         font-size: 18px;
         margin-bottom: 10px;
       }
-      .content{
+      .content {
         text-indent: 28px;
         font-size: 14px;
       }
     }
   }
-
 }
 </style>
