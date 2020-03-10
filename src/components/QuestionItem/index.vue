05cd13f7415bea8440760cc975cd1099896aa923
mahy50 <934037783@qq.com>
2020-03-10 16:32:08 +0800

    update; 更新question item

--- src/components/QuestionItem/index.vue
+++ src/components/QuestionItem/index.vue
@@ -2,58 +2,119 @@
   <div class="item flex align-center item">
     <div class="top flex align-center">
       <i class="iconfont icon-wenjuan my-icon" />
-      <div class="info">
-        <div class="title">{{ detail.title }}</div>
-        <div class="other flex align-center justify-between">
-          <!-- <div class="collect" :class="collect?'active':''" @click.stop="handelCollect">
-            <i class="iconfont icon-shoucang1" />
-            {{ collect?'取消收藏':'收藏' }}
-          </div> -->
-          <!-- <div class="share flex align-center">
-              <i class="iconfont icon-fenxiang" />
-              分享问卷
-          </div>-->
-        </div>
-      </div>
+      <el-link @click.stop="viewDetail">{{ detail.title }}</el-link>
     </div>
     <div class="status flex align-center justify-between">
-      <el-tag type="success" effect="dark">进行中</el-tag>
-      <!-- <div class="text">已收集问卷2份</div> -->
-      <div class="collect" @click="shareSurvey">收集问卷</div>
+      <el-tag v-if="detail.status === 1" type="success" effect="dark">进行中</el-tag>
+      <el-tag v-if="detail.status === 0" type="danger" effect="dark">已结束</el-tag>
+      <el-link
+        v-if="detail.share === 1"
+        type="primary"
+        :underline="false"
+        @click.stop.prevent="shareSurvey"
+      >
+        <i class="iconfont icon-fenxiang share-icon" />分享问卷
+      </el-link>
+      <el-link v-else type="primary" :underline="false" @click.stop.prevent="shareLink">复制链接</el-link>
     </div>
     <div class="fromcourse flex align-center justify-between">
-      <div class="course">
-        《{{ detail.ctitle }}》
-      </div>
+      <div class="course">课程：《{{ detail.ctitle }}》</div>
       <div v-if="!detail.start || !detail.end">不限时</div>
       <div v-else class="date">{{ detail.start }}&nbsp;至&nbsp;{{ detail.end }}</div>
     </div>
+
+    <el-dialog title="问卷链接及二维码" :visible.sync="dialogVisible" width="30%">
+      <div class="survey-link flex">
+        <canvas id="qrcode" ref="qrcode" class="qrcode" />
+        <div class="survey-link-right">
+          <el-input :value="surveyUrl" disabled>
+            <el-button slot="append" v-clipboard:copy="surveyUrl" v-clipboard:success="onCopy">复制</el-button>
+          </el-input>
+          <div class="survey-link-button">
+            <el-link class="download" :underline="false" @click="downloadQRCode">下载二维码</el-link>
+          </div>
+        </div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { shareSurvey } from '@/api/survey'
+import QRCode from 'qrcode'
 export default {
   name: 'QuestionItem',
   props: {
     detail: {
       type: Object,
       default: () => {}
+    },
+    deletable: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
     return {
+      dialogVisible: false,
       collect: false
     }
   },
+  computed: {
+    surveyUrl() {
+      const hostname = window.location.hostname
+      const port = window.location.port
+      return `http://${hostname}:${port}/#/survey/detail?id=${this.detail.id}`
+    }
+  },
   methods: {
-    handelCollect() {
-      this.collect = !this.collect
+    viewDetail() {
+      this.$router.push({
+        path: '/survey/detail',
+        query: {
+          id: this.detail.id
+        }
+      })
     },
     shareSurvey() {
       shareSurvey({ qid: 1 }).then(res => {
         console.log(res)
       })
+    },
+    shareLink() {
+      this.dialogVisible = true
+      this.$nextTick(() => {
+        this.genQRCode()
+      })
+    },
+    onCopy(e) {
+      this.$message.success('复制成功')
+    },
+    genQRCode() {
+      const node = this.$refs.qrcode
+      QRCode.toCanvas(node, this.surveyUrl, function(error) {
+        if (error) console.error(error)
+        console.log('qrcode success!')
+      })
+    },
+    downloadQRCode() {
+      const qrcode = this.$refs.qrcode
+      const MIME_TYPE = 'image/png'
+
+      const imgURL = qrcode.toDataURL(MIME_TYPE)
+
+      const dlLink = document.createElement('a')
+      dlLink.download = this.detail.title
+      dlLink.href = imgURL
+      dlLink.dataset.downloadurl = [
+        MIME_TYPE,
+        dlLink.download,
+        dlLink.href
+      ].join(':')
+
+      document.body.appendChild(dlLink)
+      dlLink.click()
+      document.body.removeChild(dlLink)
     }
   }
 }
@@ -61,14 +122,12 @@ export default {
 
 <style lang="scss" scoped>
 .item {
-  cursor: pointer;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 30px;
   padding: 20px;
   border-radius: 4px;
-  border: 1px solid rgba(181, 181, 181, 1);
-  margin-bottom: 30px;
-  width: 100%;
-  flex-direction: column;
-  // @include response-itemWidth();
+  border: 1px solid #b5b5b5;
   .top {
     width: 100%;
     height: 100%;
@@ -76,43 +135,6 @@ export default {
       color: $primaryColor;
       font-size: 44px;
       margin-right: 20px;
-    }
-    .info {
-      width: 100px;
-      flex-grow: 1;
-      .title {
-        width: 100%;
-        font-size: 14px;
-        color: $textPrimary;
-        @include ellipsis;
-      }
-      .other {
-        font-size: 12px;
-        margin-top: 10px;
-        height: 20px;
-        .date {
-          color: $textSecondary;
-        }
-        .collect,
-        .share {
-          color: $textSecondary;
-          line-height: 20px;
-          width: 80px;
-          i {
-            font-size: 20px;
-            color: $textPrimary;
-            margin-right: 2px;
-          }
-        }
-        .active {
-          i {
-            color: #ffec0f;
-          }
-        }
-        .share {
-          color: $primaryColor;
-        }
-      }
     }
   }
   .status {
@@ -125,11 +147,9 @@ export default {
       height: 25px;
       line-height: 25px;
     }
-    .text {
-      color: $textSecondary;
-    }
-    .collect {
-      color: $primaryColor;
+    .share-icon {
+      margin-right: 3px;
+      vertical-align: -1px;
     }
   }
   .fromcourse {
@@ -145,6 +165,26 @@ export default {
     }
     .data {
       width: 156px;
+    }
+  }
+  .survey-link {
+    display: flex;
+
+    .qrcode {
+      width: 100px;
+      height: 100px;
+      margin-right: 20px;
+      border: 1px solid #efefef;
+    }
+    .survey-link-right {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    .survey-link-button {
+      padding-bottom: 10px;
     }
   }
 }
